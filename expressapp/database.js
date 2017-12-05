1713f63d9721af0912e6b8ce47d9935aad49a9c2
bongsokkang <bongsokkang@gmail.com>
2017-12-04 23:18:46 -0800

    Add files via upload

---
+++ expressapp/database.js
@@ -0,0 +1,23 @@
+var characters = [
+    {
+      Name: "hello",
+      Age: 5,
+      role: 8,
+      age: 9,
+      forcePoints: 12
+    },
+    {
+      roName: "darthmaul",
+      name: "Darth Maul",
+      role: "Sith Lord",
+      age: 200,
+      forcePoints: 1200
+    },
+    {
+      routeName: "obiwankenobi",
+      name: "Obi Wan Kenobi",
+      role: "Jedi Master",
+      age: 55,
+      forcePoints: 1350
+    }
+  ];
